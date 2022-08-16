1cc43ec2e4be3c4407e5900abac20a738a599091
Youdan Zhang <gloria2612@outlook.com>
2022-08-16 16:09:33 -0400

    Create Fast_slow.java

---
+++ TwoPointers/Fast_slow.java
@@ -0,0 +1,11 @@
+boolean hasCycle(ListNode head) {
+    ListNode fast, slow;
+    fast = slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+        
+        if (fast == slow) return true;
+    }
+    return false;
+}//determine whether a ListNode has cycle
