c0e82dff4c8fef0f05caf55d5f5c00922c897d12
Youdan Zhang <gloria2612@outlook.com>
2022-08-22 12:52:49 -0400

    Create 117. Populating Next Right Pointers in Each Node II.java

---
+++ Graph/BFS/117. Populating Next Right Pointers in Each Node II.java
@@ -0,0 +1,50 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int sz = q.size();
+            Node pre = null;
+            for(int i = 0 ;i<sz;i++){
+                Node cur = q.poll();
+                if(pre!=null){
+                    pre.next = cur;
+                    
+                }
+                pre = cur;
+                 if (cur.left != null) {
+                    q.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    q.offer(cur.right);
+                }
+            }
+        }
+        return root;
+    }
+    
+}
