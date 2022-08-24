8c5e0c671e6a232c537c6a6ae2aa03efe2eb9b74
Youdan Zhang <gloria2612@outlook.com>
2022-08-23 22:19:24 -0400

    Create 78. Subsets(backtracking).java

---
+++ Graph/DFS/78. Subsets(backtracking).java
@@ -0,0 +1,17 @@
+class Solution {
+   List<List<Integer>> res = new LinkedList<>();
+LinkedList<Integer> track = new LinkedList<>();
+public List<List<Integer>> subsets(int[] nums) {
+    backtrack(nums, 0);
+    return res;
+}
+
+void backtrack(int[] nums, int start) {
+    res.add(new LinkedList<>(track));
+    for (int i = start; i < nums.length; i++) {
+        track.addLast(nums[i]);
+        backtrack(nums, i + 1);
+        track.removeLast();
+    }
+}
+}
