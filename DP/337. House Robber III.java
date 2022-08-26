c15f0e88ba7cc60464172c9a9df517f121f58a52
Youdan Zhang <gloria2612@outlook.com>
2022-08-26 01:15:34 -0400

    Create 337. House Robber III.java

---
+++ DP/337. House Robber III.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int ans[] = robHouse(root);
+        return Math.max(ans[0],ans[1]);
+    }
+    
+    public int[] robHouse(TreeNode root){
+        if(root==null){
+            return new int[2];
+        }
+        
+        int left[] = robHouse(root.left);
+        int right[] = robHouse(root.right);
+        
+        int ans[] = new int[2];
+        
+        ans[0] = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        ans[1] = root.val+left[0]+right[0];
+        
+        return ans;
+    }
+}
