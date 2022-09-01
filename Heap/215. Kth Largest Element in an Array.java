8965bc12694bac158cd5bc6adc23e7cbc023da06
Youdan Zhang <gloria2612@outlook.com>
2022-09-01 01:32:32 -0400

    Create 215. Kth Largest Element in an Array.java

---
+++ Heap/215. Kth Largest Element in an Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> n1 - n2);
+        for(int i = 0 ;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(pq.size()>k)pq.poll();
+        }
+        return pq.poll();
+    }
+}
