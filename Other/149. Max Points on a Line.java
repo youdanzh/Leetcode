fa8dfa988944b2bc850b6b3646b6d3309459533b
Youdan Zhang <gloria2612@outlook.com>
2022-08-31 21:26:55 -0400

    Create 149. Max Points on a Line.java

---
+++ Other/149. Max Points on a Line.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        
+    }
+}
+class Solution {
+    public int maxPoints(int[][] points) {
+        int res = 0;
+        for (int i = 0; i < points.length; ++i) {
+            Map<Map<Integer, Integer>, Integer> m = new HashMap<>();
+            int duplicate = 1;
+            for (int j = i + 1; j < points.length; ++j) {
+                if (points[i][0] == points[j][0] && points[i][1] == points[j][1]) {
+                    ++duplicate; continue;
+                }
+                int dx = points[j][0] - points[i][0];
+                int dy = points[j][1] - points[i][1];
+                int d = gcd(dx, dy);
+                Map<Integer, Integer> t = new HashMap<>();
+                t.put(dx / d, dy / d);
+                m.put(t, m.getOrDefault(t, 0) + 1);
+            }
+            res = Math.max(res, duplicate);
+            for (Map.Entry<Map<Integer, Integer>, Integer> e : m.entrySet()) {
+                res = Math.max(res, e.getValue() + duplicate);
+            }
+        }
+        return res;
+    }
+    public int gcd(int a, int b) {
+        return (b == 0) ? a : gcd(b, a % b);
+    }
+}
